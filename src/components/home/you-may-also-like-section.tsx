20bd93add8aa368dee2ef6faf8042affe538fb82
Spielberg Hanielly <spielberg.dev@gmail.com>
2025-08-04 11:07:56 -0300

    feat: add YouMayAlsoLikeSection component

---
+++ src/components/home/you-may-also-like-section.tsx
@@ -0,0 +1,46 @@
+import { ProductCard } from '@/components/product-card'
+import { Carousel } from '@/components/ui/carousel'
+import { getYouMayAlsoLikeProducts } from '@/hooks/getYouMayAlsoLikeProducts'
+import { cn } from '@/lib/utils'
+
+export const YouMayAlsoLikeSection = async () => {
+  const products = await getYouMayAlsoLikeProducts()
+
+  return (
+    <div className="flex w-full max-w-[75rem] flex-col gap-y-4">
+      <div className="flex flex-row gap-x-8 px-4 md:px-0">
+        <p className="font-rubik text-2xl font-extrabold md:text-[2rem]">
+          You may also like
+        </p>
+
+        <button
+          type="button"
+          className="hidden cursor-pointer px-4 py-2 md:block"
+        >
+          <p className="text-sm">View all</p>
+        </button>
+      </div>
+
+      <Carousel>
+        {products.map((product, index) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+            className={cn('ml-4 md:mr-4 md:ml-0', {
+              'md:mr-0': index === products.length - 1,
+            })}
+          />
+        ))}
+      </Carousel>
+
+      <div className="px-4 md:hidden">
+        <button
+          type="button"
+          className="w-full cursor-pointer border border-white px-4 py-1.5"
+        >
+          <p className="text-sm">View all</p>
+        </button>
+      </div>
+    </div>
+  )
+}
